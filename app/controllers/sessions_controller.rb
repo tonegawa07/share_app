373c3d7bf640fa6c5e2a5925e36772f6af122ea3
tonegawa07 <fukudayusuke07@gmail.com>
2020-12-02 01:00:45 +0900

    Add account activation

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -13,7 +13,7 @@ class SessionsController < ApplicationController
       else
         message   = "Account not activated."
         message += "Check your email for the activation link."
-        flash[;warning] = message
+        flash[:warning] = message
         redirect_to root_url
       end
     else
